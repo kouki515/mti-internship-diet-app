6a296e160c2c5114bd92d76259eb414518b90e90
kouki515 <71614240+kouki515@users.noreply.github.com>
2023-08-08 16:31:00 +0900

    modified test case

--- lambda/team4-diet-app-user-create.js
+++ lambda/team4-diet-app-user-create.js
@@ -46,21 +46,23 @@ exports.handler = async (event, context) => {
     connection.query(insertSqlCommand, function(error, results, fields) {
       if (error) {
         throw new Error("MySQL Insert Error");
+      } else {
+        console.log("ぐりいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいん")
       }
     });
 
-    const selectSqlCommand = `SELECT id FROM "${mysqlTableName}" WHERE email = "${mailaddress}" LIMIT 1`;
+    // const selectSqlCommand = `SELECT id FROM "${mysqlTableName}" WHERE email = "${mailaddress}" LIMIT 1`;
 
     // exec select
-    connection.query(selectSqlCommand, function(error, results, fields) {
-      const userId = results[0].id;
-      if (error || userId) {
-        throw new Error("MySQL Select Error");
-      } else {
-        response.statusCode = 201;
-        response.body = JSON.stringify({ userId, token });
-      }
-    });
+    // connection.query(selectSqlCommand, function(error, results, fields) {
+    //   const userId = results[0].id;
+    //   if (error || userId) {
+    //     throw new Error("MySQL Select Error");
+    //   } else {
+    //     response.statusCode = 201;
+    //     response.body = JSON.stringify({ userId, token });
+    //   }
+    // });
     
   } catch (e) {
     response.statusCode = 500;
