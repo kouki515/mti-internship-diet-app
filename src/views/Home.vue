94d62107dfaf36f196f529185f9c7edc9ae24615
kimuraryota <fransisc2035@gmail.com>
2023-08-09 18:20:51 +0900

    feature/kimura

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,47 +1,39 @@
 <template>
   <div>
     <div class="ui main container">
-      <!-- 基本的なコンテンツはここに記載する -->
-      <div class="ui segment">
-        <form class="ui form">
-          <label>ユーザーID</label>
-          <div class="inline fields">
-            <div class="field">
-              <input v-model="search.userId" type="text" name="agestart" />
+        <body>
+            <div class="ui container">
+                <h1 class="ui center aligned header">目標達成率</h1>
+                <div class="ui segment">
+                    <h2 class="ui header">ダイエッター</h2>
+                    <div class="ui field">
+                        <label>氏名：</label>
+                    </div>
+                </div>
+                <div class="ui segment">
+                    <h2 class="ui header">進捗グラフ</h2>
+                    <canvas id="weightChart"></canvas>
+                    <div class="ui center aligned statistic">
+                        <div class="value" id="percentageValue">0%</div>
+                        <div class="label">目標達成率</div>
+                    </div>
+                </div>
+                <div class="ui segment">
+                    <h2 class="ui header">履歴</h2>
+                    <table class="ui celled table">
+                        <thead>
+                            <tr>
+                                <th>日付</th>
+                                <th>目標達成率</th>
+                            </tr>
+                        </thead>
+                        <tbody id="progressTableBody">
+                            <!-- ここにデータが追加されます -->
+                        </tbody>
+                    </table>
+                </div>
             </div>
-          </div>
-          <div class="inline fields">
-            <div class="field">
-              <div for="ageend">カテゴリー名</div>
-              <input v-model="search.category" type="text" name="agestart" />
-            </div>
-          </div>
-          <div class="inline fields">
-            <div class="field">
-              <div>投稿日時</div>
-              <input v-model="start" type="text" name="agestart" />
-              <label for="agestart">から</label>
-            </div>
-            <div class="field">
-              <input v-model="end" type="text" name="ageend" />
-              <label for="ageend">まで</label>
-            </div>
-          </div>
-        </form>
-      </div>
-      <div class="ui segment">
-        <!--<Posts :post_array = "post_array" />-->
-        <ul class="ui three column grid">
-          <template v-for="(item, index) in articles" :key="index">
-            <li class="column">
-              <h2 class="header">{{ item.userId }}</h2>
-              <span>{{ convertToLocaleString(item.timestamp) }}</span>
-              <div>{{ item.text }}</div>
-              <div class="ui green label">{{ item.category }}</div>
-            </li>
-          </template>
-        </ul>
-      </div>
+        </body>
     </div>
   </div>
 </template>
@@ -51,7 +43,7 @@
 // @は/srcと同じ意味です
 // import something from '@/components/something.vue';
 import { baseUrl } from "@/assets/config.js";
-import Posts from "@/components/Posts.vue";
+
 
 // const headers = {'Authorization' : 'mtiToken'};
 
@@ -60,68 +52,39 @@ export default {
 
   components: {
     // 読み込んだコンポーネント名をここに記述する
-    Posts,
+   
   },
 
   data() {
     // Vue.jsで使う変数はここに記述する
     return {
-      post: {
-        text: null,
-        category: null,
-      },
-      search: {
-        userId: null,
-        category: null,
-        start: null,
-        end: null,
-      },
-      articles: [],
-      iam: null,
+      logs: [],
+      targetWeight: "",
+      latestWeight: "",
+      user_id: window.localStorage.getItem("userId"),
+      name: "",
     };
   },
   computed: {
-    // 計算した結果を変数として利用したいときはここに記述する
-    //   async getSearchedArticle() { // 記事を検索する
-    //   return this.articles.filter(e =>
-    //     e.userId?.match(e.search.userId)
-    //     && convertToLocalString(e.timestamp) >= this.search.start
-    //     && convertToLocalString(e.timestamp) <= this.search.end
-    //   );
-    // },
-    async filteredArticles() {
-      return this.articles.filter(e =>
-          e.userId?.match(this.search.userId)
-          && e.timestamp >= new Date(this.search.start)
-          && e.timestamp <= new Date(this.search.end)
-      );
-    }
+   
   },
 
   methods: {
     // Vue.jsで使う関数はここで記述する
-    // isMyArticle(id) {}, // 自分の記事かどうかを判定する
-    // async getArticles() {}, // 記事一覧を取得する
+    calculateAchievement(weight, target , first) {
+      let achievement = (((first - weight)/(first - target)) * 100).toFixed(1); 
+      return achievement;
+    },
 
-    // async postArticle() {}, // 記事を作成する
+    
+    async fetchTargetWeight(){
+      const headers = { Authorization: window.localStorage.getItem("token") };
+      if (headers.Authorization=="5da50cf80844d2cdab1bc15aa9e64802c7d365dec585b2896b9ef46f4274bfc2") {
+      this.$router.push("/login");
+      return;
+    }
 
-    // async deleteArticle(article) {}, // 記事を削除する
-    convertToLocaleString(timestamp) {
-      let time = new Date(timestamp);
-      let date = time.toLocaleDateString();
-      let date_2 = time.toLocaleTimeString();
-      return date + ' ' + date_2;
-    }, // timestampをLocaleDateStringに変換する
-  },
-
-  created: async function () {
-    // Vue.jsの読み込みが完了したときに実行する処理はここに記述する
-    // apiからarticleを取得する
-    const headers = { Authorization: "mtiToken" };
-
-    try {
-      /* global fetch */
-      const res = await fetch(baseUrl + `/articles`, {
+      const res = await fetch(`${baseUrl}/weightgoal?user_id=${this.userId}`, {
         method: "GET",
         headers,
       });
@@ -129,17 +92,50 @@ export default {
       const text = await res.text();
       const jsonData = text ? JSON.parse(text) : {};
 
-      // faild
+      
       if (!res.ok) {
-        const errorMessage = jsonData.message ?? "error message not found";
+        const errorMessage = jsonData.message ?? "エラーメッセージがありません";
         throw new Error(errorMessage);
       }
 
-      // success
-      this.articles = jsonData.articles;
-    } catch (e) {
-      // error
+      this.targetWeight = jsonData;
+      console.log(this.targetWeight);
+    },
+
+    async fetchLogs(){
+      const headers = { Authorization: window.localStorage.getItem("token") };
+      if (headers.Authorization == "5da50cf80844d2cdab1bc15aa9e64802c7d365dec585b2896b9ef46f4274bfc2") {
+      this.$router.push("/login");
+      return;
     }
+
+      const res = await fetch(`${baseUrl}/logs?user_id=${this.userId}`, {
+        method: "GET",
+        headers,
+      });
+
+      const text = await res.text();
+      const jsonData = text ? JSON.parse(text) : {};
+
+      
+      if (!res.ok) {
+        const errorMessage = jsonData.message ?? "エラーメッセージがありません";
+        throw new Error(errorMessage);
+      }
+
+      this.firstWeight = jsonData[jsonData.length - 1].weight_log;
+
+      this.logs = jsonData;
+      console.log(this.logs)
+      console.log(this.firstWeight)
+    },
+
+  },
+  
+  created: async function () {
+    // Vue.jsの読み込みが完了したときに実行する処理はここに記述する
+    // apiからarticleを取得する
+    await this.fetchLogs();
   },
 };
 </script>
