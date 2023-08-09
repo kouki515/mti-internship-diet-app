5179cbe417038624a397aa6a88a487f6531a550f
Yabu624 <yuuki_624@icloud.com>
2023-08-09 13:11:00 +0900

    それっぽくまとめました

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -5,11 +5,17 @@
       <header v-if="isLogin">
         <h1>LOGIN</h1>
       </header>
-      <div class="ui segment" v-if="isLogin">
+      <h1 class="ui center aligned header" v-if="isLogin == false">サインアップ</h1>
+
+      <div class="ui segment">
         <h4>必要なログイン情報をご入力ください。</h4>
         <form class="ui large form">
           <div class="field">
             <div class="t">
+              <div class="field" v-if="isLogin == false">
+                <label for="username">ユーザーネーム：</label>
+                <input type="text" id="username" name="username" required v-model="users.name">
+              </div>
               <label for="">メールアドレス</label>
               <input type="email" placeholder="12345@example.com" v-model=users.mailaddress required>
             </div>
@@ -18,92 +24,12 @@
               <input type="password" maxlength="15" pattern="^[a-zA-Z0-9]+$" v-model=users.password required>
             </div>
           </div>
-          <div class="field">
-            <div class="ui checkbox">
-              <input type="checkbox">
-              <label for="">パスワードを保存する</label>
-            </div>
-          </div>
-          <button class="ui olive fluid button" type="submit" @click="submit()">ログイン</button>
+          <button class="ui olive fluid button" type="submit" @click="submit()">{{ submitText }}
+          </button>
         </form>
         <div class="y">
-          <button class="ui black basic fluid button" type="button" @click="DieterMode()">登録</button>
-        </div>
-        <!-- <div class="s">
-          <a href="" class="a">ログインでお困りの方はこちら</a>
-        </div> -->
-        <div>
-          <button class="ui black basic fluid button" type="button" @click="WatcherMode()">合言葉をお持ちの方はこちら</button>
-        </div>
-      </div>
-    </div>
-    <!-- dieterの新規登録 -->
-    <div class="ui container">
-      <div class="ui segment" v-if="isDiet">
-        <h1 class="ui center aligned header">サインアップ（ダイエッター）</h1>
-        <form class="ui form">
-          <div class="field">
-            <label for="email">メールアドレス：</label>
-            <input type="email" id="email" name="email" required v-model="users.mailaddress">
-          </div>
-          <div class="field">
-            <label for="password">パスワード：</label>
-            <input type="password" id="password" name="password" required v-model="users.password">
-          </div>
-          <div class="field">
-            <label for="username">ユーザーネーム：</label>
-            <input type="text" id="username" name="username" required v-model="users.name">
-          </div>
-          <!-- <div class="field">
-            <label for="height">身長（cm）：</label>
-            <input type="number" id="height" name="height" v-model.number="users.height">
-          </div>
-          <div class="field">
-            <label for="weight">体重（kg）：</label>
-            <input type="number" id="weight" name="weight" v-model="users.weight">
-          </div> -->
-          <!-- <div class="field">
-            <label for="weight">目標体重（kg）：</label>
-            <input type="number" id="weight" name="weight" v-model="users.weight_goal">
-          </div> -->
-          <!-- <div class="field">
-            <label for="age">年齢：</label>
-            <input type="number" id="age" name="age" required v-model.number="users.age">
-          </div>
-          <div class="field">
-            <label for="secret_word">合言葉：</label>
-            <input type="password" id="secret_word" name="secret_word" placeholder="合言葉を決めてください" required
-              v-model="users.secret_word">
-          </div> -->
-          <button class="ui olive fluid button" type="submit" @click="submit()">サインアップ</button>
-        </form>
-      </div>
-    </div>
-    <!-- watcherの新規登録ページ -->
-    <div class="ui main container">
-      <div class="ui container">
-        <div class="ui segment" v-if="isWatch">
-          <h1 class="ui center aligned header">サインアップ（ウォッチャー）</h1>
-          <form class="ui form">
-            <div class="field">
-              <label for="email">メールアドレス：</label>
-              <input type="email" id="email" name="email" v-model="users.mailaddress" required>
-            </div>
-            <div class="field">
-              <label for="password">パスワード：</label>
-              <input type="password" id="password" name="password" v-model="users.password" required>
-            </div>
-            <div class="field">
-              <label for="username">ユーザーネーム：</label>
-              <input type="text" id="username" name="username" v-model="users.name" required>
-            </div>
-            <!-- <div class="field">
-              <label for="secret_word">合言葉：</label>
-              <input type="password" id="secret_word" name="secret_word" placeholder="教えてもらった合言葉を入れて下さい"
-                v-model="users.secret_word" required>
-            </div> -->
-            <button class="ui olive fluid button" type="submit" @click="submit()">サインアップ</button>
-          </form>
+          <button class="ui black basic fluid button" type="button" @click="DieterMode()">{{ toggleText }}
+          </button>
         </div>
       </div>
     </div>
@@ -126,35 +52,33 @@ export default {
   data() {
     // Vue.jsで使う変数はここに記述する
     return {
-      isDiet: false,
-      isWatch: false,
       isLogin: true,
       // ダイエットする人に必要な変数
       users: {
-        // userId: "test",
         id: Number,
         mailaddress: "",
         password: "",
         name: "",
-        // height: 170,
-        // weight: 60,
-        // weight_goal: 50,
-        // age: 22,
-        // secret_word: "test",
       }
     };
   },
 
   computed: {
     // 計算した結果を変数として利用したいときはここに記述する
+    submitText() {
+      return this.isLogin ? 'ログイン' : '新規登録';
+    },
+    toggleText() {
+      return this.isLogin ? '新規登録' : 'ログイン';
+    }
   },
 
   methods: {
     // Vue.jsで使う関数はここで記述する
     async submit() {
 
-      // dieterの新規登録
-      if (this.isDiet && this.isLogin == false && this.isWatch == false) {
+      // 新規登録
+      if (this.isLogin == false) {
 
         // リクエストボディを指定する
         const requestBody = {
@@ -194,48 +118,8 @@ export default {
         }
       }
 
-      // watcherの新規登録時に実行
-      else if (this.isWatch && this.isDiet == false && this.isLogin == false) {
-        // リクエストボディを指定する
-        const headers = { 'Authorization': '5da50cf80844d2cdab1bc15aa9e64802c7d365dec585b2896b9ef46f4274bfc2' };
-
-        const requestBody = {
-          name: this.users.name,
-          mailaddress: this.users.mailaddress,
-          password: this.users.password,
-        }
-
-        try {
-          /* global fetch */
-          const res = await fetch(baseUrl + '/user/signup',
-            {
-              method: 'POST',
-              body: JSON.stringify(requestBody),
-              headers
-            });
-          console.log(this.requestBody);
-
-          const text = await res.text();
-          const jsonData = text ? JSON.parse(text) : {}
-
-          // fetchではネットワークエラー以外のエラーはthrowされないため、明示的にthrowする
-          if (!res.ok) {
-            const errorMessage = jsonData.message ?? 'エラーメッセージがありません';
-            throw new Error(errorMessage);
-          }
-
-          // 成功時の処理
-          window.localStorage.setItem('token', jsonData.token);
-          window.localStorage.setItem('userId', this.users.userId);
-
-          // this.$router.push({ name: 'Home' })
-        } catch (e) {
-          // エラー時の処理
-        }
-      }
-
       // ログイン時の処理
-      else if (this.isLogin && this.isDiet == false && this.isWatch == false) {
+      else if (this.isLogin) {
         // リクエストボディを指定する
 
         const requestBody = {
@@ -275,15 +159,7 @@ export default {
 
     // dieterの新規登録の表示
     DieterMode() {
-      this.isDiet = true;
-      this.isLogin = false;
-      this.isWatch = false;
-    },
-    // watcherの新規登録の表示
-    WatcherMode() {
-      this.isWatch = true;
-      this.isLogin = false;
-      this.isDiet = false;
+      this.isLogin = !this.isLogin;
     },
   },
 }
